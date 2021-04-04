7857091f6d8bddbfd5834c445f0317f995bb8df2
GIRONE <gironeremi@gmail.com>
2021-04-04 18:03:50 +0200

    Mise en place du CSS, initialisation du projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    </head>
+    <body>
+        <header>
+            <h1>Bienvenue!</h1>
+        </header>        
+        <section>
+
+        </section>
+        <footer>
+
+        </footer>
+    </body>
+</html>
